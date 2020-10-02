cf749121fc49af183f7914329505331c708faa03
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-10-02 11:11:46 +0200

    added basic page structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
 
+const calendarDates = Array(31)
+  .fill(0)
+  .map((e, i) => i);
+
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <div className='date-chooser'>
+        <button className='date-chooser-button'>
+          Start Date <span>0</span>
+        </button>
+        <button className='date-chooser-button'>
+          End Date <span>0</span>
+        </button>
+      </div>
+
+      <div className='calendar'>
+        {calendarDates.map((day, index) => {
+          return <div className='calendar-day'>{day + 1}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
