f6f454f96673344f2a43cc635803a7b52cdd0ec6
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-10-02 11:59:46 +0200

    added date picker functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const calendarDates = Array(31)
   .fill(0)
   .map((e, i) => i);
 
-function App() {
+const App = () => {
+  const [choosingType, setChosingType] = useState('start'); // 'start' or 'end'
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const updateDate = (chosenDay) => {
+    if (startDate && chosenDay < startDate) {
+      setStartDate(chosenDay);
+      return setChosingType('end');
+    }
+
+    if (endDate && chosenDay > endDate) {
+      setEndDate(chosenDay);
+      return setChosingType('end');
+    }
+    if (choosingType === 'start') {
+      setStartDate(chosenDay);
+      return setChosingType('end');
+    }
+
+    if (choosingType === 'end') {
+      setEndDate(chosenDay);
+    }
+  };
   return (
     <div className='app'>
       <div className='date-chooser'>
-        <button className='date-chooser-button'>
-          Start Date <span>0</span>
+        <button
+          onClick={() => setChosingType('start')}
+          className='date-chooser-button'
+        >
+          Start Date <span>{startDate}</span>
         </button>
-        <button className='date-chooser-button'>
-          End Date <span>0</span>
+        <button
+          onClick={() => setChosingType('end')}
+          className='date-chooser-button'
+        >
+          End Date <span>{endDate}</span>
         </button>
       </div>
 
       <div className='calendar'>
         {calendarDates.map((day, index) => {
-          return <div className='calendar-day'>{day + 1}</div>;
+          const dayNumber = day + 1;
+          return (
+            <button
+              key={index}
+              onClick={() => updateDate(dayNumber)}
+              className='calendar-day'
+            >
+              {dayNumber}
+            </button>
+          );
         })}
       </div>
     </div>
   );
-}
+};
 
 export default App;
